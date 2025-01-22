dc7ca63954e6d694bff42cb57696bebc6f6fb0f6
KarhuPumppu <miro.airas@outlook.com>
2025-01-22 14:06:37 +0200

    deleted:    src/app/services/antiCheat.js

--- src/app/services/antiCheat.js
+++
@@ -1,9 +0,0 @@
-export default function disableEnter(){
-    document
-      .getElementById("btn")
-      .addEventListener("keydown", function (event) {
-        if (event.key === "Enter") {
-          event.preventDefault();
-        }
-      });
-}
